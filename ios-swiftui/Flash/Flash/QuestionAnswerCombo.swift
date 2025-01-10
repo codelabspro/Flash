2511d36f88f2477b3c0f247b16fc1dd524210284
Arunabh Das <arunabhdas@gmail.com>
2025-01-10 04:57:01 -0500

    Added initializer to QuestionAnswerCombo

--- ios-swiftui/Flash/Flash/QuestionAnswerCombo.swift
+++ ios-swiftui/Flash/Flash/QuestionAnswerCombo.swift
@@ -17,5 +17,40 @@ class QuestionAnswerCombo: Codable {
     var dateAdded: Date
     var dateAnswered: Date
     var summary: String
-    var difficultyRating: Int
+    var difficultyRating: Int?
+    var status: Status
+    
+    init(question: String, optionA: String, optionB: String, optionC: String, optionD: String, correctOption: String, dateAdded: Date, dateAnswered: Date, summary: String, difficultyRating: Int, status: Status) {
+        self.question = question
+        self.optionA = optionA
+        self.optionB = optionB
+        self.optionC = optionC
+        self.optionD = optionD
+        self.correctOption = correctOption
+        self.dateAdded = dateAdded
+        self.dateAnswered = dateAnswered
+        self.summary = summary
+        self.difficultyRating = difficultyRating
+        self.status = status
+    }
+}
+
+
+enum Status: Int, Codable, Identifiable, CaseIterable {
+    case none, unanswered, answered
+    
+    var id: Self {
+        self
+    }
+    
+    var descr: String {
+        switch self {
+        case .none:
+            return "None"
+        case .unanswered:
+            return "Unanswered"
+        case .answered:
+            return "Answered"
+        }
+    }
 }
---
+++ ios-swiftui/Flash/Flash/Untitled.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  Flash
+//
+//  Created by Coder on 1/10/25.
+//
+
